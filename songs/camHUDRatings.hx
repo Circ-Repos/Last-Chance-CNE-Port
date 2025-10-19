9014f0765e603e866edd3c0fe0badd3caee52897
Circuitella <116322307+Circuitella@users.noreply.github.com>
2025-10-19 14:00:19 -0400

    Thats not CamHUD Ratings

--- songs/camHUDRatings.hx
+++ songs/camHUDRatings.hx
@@ -1,38 +1,19 @@
-//big credit to vechett on CNE discord for the code
-import haxe.ds.ObjectMap;
-
-var frozenCharacters:ObjectMap<Character, Bool> = new ObjectMap();
-var frozenStrums:ObjectMap<Strum, Bool> = new ObjectMap();
 public var ratingScaleDiff:Float = 0.1;
 
-function onNoteHit(e) {
-	e.numScale -= ratingScaleDiff;
-	e.ratingScale -= ratingScaleDiff;
-
-    for (char in e.characters){
-        if (e.note.isSustainNote) frozenCharacters.set(char, true);
-        if (e.note.animation.name == "holdend") frozenCharacters.set(char, false);
-    }
-    if (e.note.isSustainNote) frozenStrums.set(strumLines.members[e.note.strumLine.ID].members[e.note.strumID], true);
-    if (e.note.animation.name == "holdend") frozenStrums.set(strumLines.members[e.note.strumLine.ID].members[e.note.strumID], false);
+function postCreate()
+{
+    comboGroup.x = 560;
+    comboGroup.y = 290;
+}
+function postUpdate()
+{
+	comboGroup.forEachAlive(function(spr) {
+		if(spr.camera != camHUD) spr.camera = camHUD;
+	});
 }
 
-function postUpdate(elapsed:Float) {
-    for (char in frozenCharacters.keys()) {
-        var bool = frozenCharacters.get(char);
-        if (char.animateAtlas == null) char.animation.paused = bool;
-        else {
-            var isPaused = char.animateAtlas.anim.paused;
-            if (bool && !isPaused) char.animateAtlas.anim.pause();
-            else if (!bool && isPaused) char.animateAtlas.anim.play();
-        }
-    }
-    for (char in frozenStrums.keys()) char.animation.paused = frozenStrums.get(char);
+function onNoteHit(event)
+{
+	event.numScale -= ratingScaleDiff;
+	event.ratingScale -= ratingScaleDiff;
 }
-
-function onPlayerMiss(e){
-    for (a in e.characters) frozenCharacters.set(a, false);
-    frozenStrums.set(strumLines.members[e.note.strumLine.ID].members[e.note.strumID], false);
-}
-
-
